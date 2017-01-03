9e0081712a2b5da165a59a0234cf03585940b550
njiang <njiang@oanda.com>
2017-01-03 16:42:21 -0500

    some formating

--- python/461/hamming_distance.py
+++ python/461/hamming_distance.py
@@ -3,19 +3,13 @@ class Solution(object):
 
     def hammingDistance(self, x, y):
         """
-
         :type x: int
-
         :type y: int
-
         :rtype: int
-
         """
-        # Convert the int to binary in string format, then compare them bit by bit
+        # Convert the int to binary in string format, then compare them bit by bit, up to 32bits
         binary_x = bin(x)[2:].zfill(32) 
         binary_y = bin(y)[2:].zfill(32)
-        print binary_x
-        print binary_y
         
         return sum( bit_in_x != bit_in_y for bit_in_x, bit_in_y in zip(binary_x, binary_y))
 
